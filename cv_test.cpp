c95e2227dc6d06ec4455c96a59fa39fc8e94e5b8
taketo <26m206@st.takushoku-u.ac.jp>
2026-05-14 10:49:42 +0900

    2026/05/14

--- cv_test.cpp
+++ cv_test.cpp
@@ -1,5 +1,41 @@
 #include <iostream>
+#include <vector>
 #include <opencv2/opencv.hpp>
+
+auto clamp = [](int val) {
+    if (val > 255) { val = 255; }
+    if (val < 0) { val = 0; }
+    return val;
+};
+
+cv::Mat rgb2YCbCr(cv::Mat img) {
+    // RGB -> YCbCr, img はシャローコピー
+    const int width   = img.cols;
+    const int height  = img.rows;
+    const int num_chn = img.channels();
+    const int stride  = img.step;
+    uint8_t* pixel    = img.data;
+
+    for (int i = 0; i < height; ++i) {
+        for (int j = 0; j < width; ++j) {
+            int B = pixel[i * stride + j * num_chn + 0];
+            int G = pixel[i * stride + j * num_chn + 1];
+            int R = pixel[i * stride + j * num_chn + 2];
+
+            int Y, Cb, Cr;
+            Y  = 0.299 * R + 0.587 * G + 0.114 * B;
+            Cb = -0.1687 * R + -0.3313 * G + 0.5 * B;
+            Cr = 0.5 * R + -0.4187 * G + -0.0813 * B;
+            Cb += 128;
+            Cr += 128;
+
+            pixel[i * stride + j * num_chn + 0] = clamp(Y);
+            pixel[i * stride + j * num_chn + 1] = clamp(Cr);
+            pixel[i * stride + j * num_chn + 2] = clamp(Cb);
+        }
+    }
+    return img;
+}
 
 int main(void) {
     std::cout << "cv_test" << std::endl;
@@ -12,26 +48,24 @@ int main(void) {
     const int stride  = image.step;
     std::cout << "width = " << width << ", height = " << height << std::endl;
 
-    auto pixel = image.data;
-    auto clamp = [](int val) {
-        if (val > 255) { val = 255; }
-        if (val < 0) { val = 0; }
-        return val;
-    };
-    auto pic = [&stride, &num_chn](const int& h, const int& w, const int& c) { return h * stride + w * num_chn + c; };
-    for (int i = 0; i < height; ++i) {
-        for (int j = 0; j < width; ++j) {
-            int B = pixel[i * stride + j * num_chn + 0];
-            int G = pixel[i * stride + j * num_chn + 1];
-            int R = pixel[i * stride + j * num_chn + 2];
+    image = rgb2YCbCr(image);
+    std::vector<cv::Mat> ycrcb;
+    cv::split(image, ycrcb);                              //[0] -> Y, [1] -> Cr, [2] -> Cb
+    cv::resize(ycrcb[1], ycrcb[1], cv::Size(), 0.5, 0.5); // 444 -> 420
+    cv::resize(ycrcb[2], ycrcb[2], cv::Size(), 0.5, 0.5); // 444 -> 420
 
-            pixel[pic(i, j, 0)] = clamp(B * 4);
-            pixel[pic(i, j, 1)] = clamp(G * 4);
-            pixel[pic(i, j, 2)] = clamp(R * 4);
+    for (int y = 0; y < height; y += 8) {
+        for (int x = 0; x < width; x += 8) {
+            cv::Mat blk;
+            ycrcb[0](cv::Rect(x, y, 8, 8)).convertTo(blk, CV_32F);
+            blk -= 128.0f;
+            // DCT
+            // 量子化
+            // エントロピー符号化
         }
     }
 
-    cv::imshow("loaded image", image);
+    cv::imshow("loaded image", ycrcb[0]);
 
     cv::waitKey(0);
     cv::destroyAllWindows();
