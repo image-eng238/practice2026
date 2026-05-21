503ccff7350fd2fb77da22ff1cb5e3acc194c056
taketo <26m206@st.takushoku-u.ac.jp>
2026-05-21 11:10:51 +0900

    課題

--- cv_test.cpp
+++ cv_test.cpp
@@ -47,6 +47,20 @@ rgb2YCbCr(cv::Mat img) {
 
 template <size_t X = 0>
 void quantize(cv::Mat& blk, const float* const qtable, const int quality = 75) {
+    auto QF = [](int q) {
+        assert(0 <= q && q <= 100);
+        q = q ? q : 1;
+        double qf;
+        if (q <= 50) {
+            qf = static_cast<int>(5000 / q);
+        } else {
+            qf = 200 - (2 * q);
+        }
+        qf = qf ? qf : 1;
+        return qf;
+    };
+
+    float Scale        = QF(75) / 100;
     const int width    = blk.cols;
     const int height   = blk.rows;
     const int num_chn  = blk.channels();
@@ -56,7 +70,7 @@ void quantize(cv::Mat& blk, const float* const qtable, const int quality = 75) {
     for (size_t i = 0; i < height; ++i) {
         for (size_t j = 0; j < width; ++j) {
             const auto val      = pixel[i * stride + j];
-            const auto stepsize = clamp(qtable[i * stride + j]);
+            const auto stepsize = clamp(qtable[i * stride + j] * Scale);
             if constexpr (X == 0) { // 量子化
                 pixel[i * stride + j] = roundf(val / stepsize);
             } else { // 逆量子化
